c3e8d9f07e79de1b9428939bac710ed352a52ca7
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2024-01-26 11:56:17 -0300

    feat: create appointment factory, fix: correction bugs with data types received in AgendaModel.Allappointments

---
+++ src/factories/AppointmentFactory.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { BodyProtocol } from "../Models/AgendaModelTypes";
+
+export type returnBuildDate = { id: string, title: string, start: Date, end: Date }
+
+type ApointmentFactoryProtocol<T> = {
+  buildDate: (simpleAppo: mongoose.Document<unknown, T>) => returnBuildDate
+}
+
+class ApointmentFactory implements ApointmentFactoryProtocol<BodyProtocol> {
+  buildDate(simpleAppo) {
+    const { date, _id, name, describe, time } = simpleAppo;
+
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate() + 1;
+    const [hour, minutes] = time.split(':')
+
+    const startDate = new Date(year, month, day, +hour - 3, +minutes, 0, 0);
+    const endDate = new Date(year, month, day, +hour - 2, +minutes, 0, 0);
+
+    const appo = {
+      id: _id,
+      title: `${name} - ${describe}`,
+      start: startDate,
+      end: endDate,
+    }
+
+    return appo;
+  }
+}
+
+export default new ApointmentFactory;
