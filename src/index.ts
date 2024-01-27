7bf111b1e2a2b6f2c00b38904c5dbee3e2b2acd8
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2024-01-27 12:06:57 -0300

    delete: comments in index

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { router as routes } from './router';
 import { connectionDB } from '../database/connection';
-// import Agenda from '../database/AgendaModel';
 config();
 
 const app = express();
@@ -15,21 +14,9 @@ app.use(bodyParser.json())
 
 app.set('view engine', 'ejs');
 
-//Connexao com Banco MongoDB:
 connectionDB()
   .then(() => {
     console.log('Success Connection!');
-
-    // Agenda.insert({
-    //   date: new Date(),
-    //   middlename: 'Alencar',
-    //   age: 27,
-    //   phoneNumber: 12345467898,
-    //   email: 'dev.alencar.gabriel@gmail.com',
-    //   name: 'Gabriel',
-    // }).then(resp => {
-    //   console.log(resp);
-    // }).catch(err => console.log(err))
 
   })
   .catch(err => console.log(err))
